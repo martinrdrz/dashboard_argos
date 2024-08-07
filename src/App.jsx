a291a6fb30c6acd829b813bc484082a9e5092912
martinrdrz <martinrdrz@hotmail.com>
2024-08-07 17:48:07 -0300

    commit antes de la modificacion del area de almacenamiento para auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { C4DashboardPage } from './pages';
 import { LoginPage } from './pages';
-//import { Provider } from 'react-redux';
-//import { store } from './store/store';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 export const App = () => {
-    const status = 'not-authenticated'; //not-authenticated,  authenticated
+    const status = 'authenticated'; //not-authenticated,  authenticated
     return (
-        // <Provider store={store}>
-        <Routes>
-            {status === 'not-authenticated' ? (
-                <>
-                    <Route path="/auth/*" element={<LoginPage />} />
-                    <Route path="/*" element={<Navigate to="/auth/login" />} />
-                </>
-            ) : (
-                <>
-                    <Route path="/*" element={<C4DashboardPage />} />
-                </>
-            )}
-        </Routes>
-        // </Provider>
+        <Provider store={store}>
+            <Routes>
+                {status === 'not-authenticated' ? (
+                    <>
+                        <Route path='/auth/*' element={<LoginPage />} />
+                        <Route path='/*' element={<Navigate to='/auth/login' />} />
+                    </>
+                ) : (
+                    <>
+                        <Route path='/*' element={<C4DashboardPage />} />
+                    </>
+                )}
+            </Routes>
+        </Provider>
     );
 };
